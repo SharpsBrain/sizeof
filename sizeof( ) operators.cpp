5db752eb66d15f03af533c8cbea95af5e235cfb4
SharpsBrain <109470818+SharpsBrain@users.noreply.github.com>
2022-07-18 22:23:26 +0500

    Add files via upload

---
+++ sizeof( ) operators.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Size of char data type= "<<sizeof(char)<<endl;
+
+    cout<<"Size of int data type= "<<sizeof(int)<<endl;
+    cout<<"Size of short data type= "<<sizeof(short)<<endl;
+    cout<<"Size of unsigned int data type= "<<sizeof(unsigned int)<<endl;
+    cout<<"Size of long data type= "<<sizeof(long)<<endl;
+    cout<<"Size of long long data type= "<<sizeof(long long)<<endl;
+    cout<<"Size of bool data type= "<<sizeof(bool)<<endl;
+    cout<<"Size of float data type= "<<sizeof(float)<<endl;
+    cout<<"Size of double data type= "<<sizeof(double)<<endl;
+    cout<<"Size of long double data type= "<<sizeof(long double)<<endl;
+    cout<<"Size of unsigned short data type= "<<sizeof(unsigned short)<<endl;
+    cout<<"Size of unsigned long data type= "<<sizeof(unsigned long)<<endl;
+    return 0;
+}
+//---------------------------size of operator--------------------------------
